e91abc2901cbadfa4d97624d8c990dc34755e482
John B <contact@johnshustle.com>
2025-11-13 14:18:58 +0100

    add health endpoint

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -9,6 +9,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors());
+
+app.get("/health", (req: Request, res: Response) => {
+  console.log("Health endpoint hit");
+  res.status(200).send("OK");
+});
 
 app.listen(7000, () => {
   console.log("Basic app running on port 7000");
